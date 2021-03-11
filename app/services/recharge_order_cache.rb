753ab31c7ca2dc91753c81d80569fcbf81ac5a4d
Floyd Wallace <flwallace105@gmail.com>
2021-03-10 17:00:35 -0800

    end of day commit

--- app/services/recharge_order_cache.rb
+++ app/services/recharge_order_cache.rb
@@ -8,8 +8,9 @@ module RechargeOrderCache
         puts "Hi in the order self module"
         page_size = 250
         url = "#{BASE_URI}/orders/count"
-        scheduled_at_min = Date.today.strftime("%Y-%m-%d")
-        scheduled_at_max = Date.today.end_of_month.strftime("%Y-%m-%d")
+        scheduled_at_min = Date.today.at_beginning_of_month.next_month.strftime("%Y-%m-%d")
+        scheduled_at_max = Date.today.end_of_month.next_month.strftime("%Y-%m-%d")
+        puts "scheduled_at_min = #{scheduled_at_min}, scheduled_at_max = #{scheduled_at_max}"
         query = { status: 'queued', scheduled_at_min: scheduled_at_min, scheduled_at_max: scheduled_at_max }
         response = HttpartyService.get(url, {}, query)
         orders_response = HttpartyService.parse_response(response)
@@ -18,6 +19,30 @@ module RechargeOrderCache
         num_pages = (orders_size / page_size.to_f).ceil
         puts "we have #{orders_size} orders"
 
+        query.merge!(limit: page_size)
+
+        1.upto(num_pages) do |page|
+            url = URI("#{BASE_URI}/orders")
+            query.merge!(page: page)
+    
+            response = HttpartyService.get(url, {}, query)
+            orders_response = HttpartyService.parse_response(response)['orders']
+            #puts orders_response.inspect
+
+            orders_response.each do |order|
+                puts "----------"
+                puts order.inspect
+                puts "----------"
+
+            end
+
+            recharge_limit = response.headers['x-recharge-limit']
+
+            puts "Done loading page #{page}"
+            BackgroundHelper.determine_limits(recharge_limit, 0.65)
+
+        end
+        puts "All done with download orders"
 
     end
 
