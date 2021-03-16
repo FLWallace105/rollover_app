a129e8a1206f6767251bcd1f8af0cf98f362a300
Floyd Wallace <flwallace105@gmail.com>
2021-03-16 13:05:31 -0700

    added nuke/pave comment to controller for selection

--- app/controllers/selection_set_controller.rb
+++ app/controllers/selection_set_controller.rb
@@ -2,4 +2,11 @@ class SelectionSetController < ApplicationController
     def index
     end
 
+    def new
+
+    end
+
+    def create
+        #should nuke and pave, delete old selection record as we should have only one at a time
+    end
 end
