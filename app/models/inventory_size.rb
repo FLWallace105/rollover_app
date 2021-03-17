96f635e98492494176d789e7895c7fe299cea880
Floyd Wallace <flwallace105@gmail.com>
2021-03-17 13:27:11 -0700

    initial take sizebreaks

--- app/models/inventory_size.rb
+++ app/models/inventory_size.rb
@@ -1,4 +1,9 @@
 class InventorySize < ApplicationRecord
+    enum product_size: [:XS, :S, :M, :L, :XL]
+    enum product_type: { "leggings" => 0, "tops" =>1, "sports-bra" => 2, "sports-jacket" => 3, "gloves" => 4}
+
+
+
     validates :product_size, inclusion: { in: %w(XS S M L XL),
         message: "%{value} is not a valid size" }
     
--- app/models/size_break.rb
+++ app/models/size_break.rb
@@ -1,4 +1,5 @@
 class SizeBreak < ApplicationRecord
+    
     #model holds size_break type info, including using gloves or not, using size break or not
     #and which out of these are used (can only use three total or four if using gloves): gloves, sports-jacket, sports-bra, tops, leggings 
 end
